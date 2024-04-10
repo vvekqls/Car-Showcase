b8c76f2de977b3236e472c70e820430a0dccbc3d
vvekqls <d3song@ucsd.edu>
2024-04-09 23:25:09 -0700

    [DS] Update tailwind config & css

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,11 @@
 import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 export {
-  Hero
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer
 }
